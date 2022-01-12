2c37c25d90eb5b1886e2bfb3e41459829ef655b8
Imre Gyori <imre.gyori77@gmail.com>
2022-01-12 19:57:02 +0100

    Renamed attributes

--- rubik_solve.py
+++ rubik_solve.py
@@ -20,40 +20,33 @@ T["BackLeft"]  =   [0,1,2,3,4,23,6,22,10,8,11,9,17,13,16,15,5,7,18,19,20,21,12,1
 T["BackRight"] =   [0,1,2,3,4,16,6,17,9,11,8,10,22,13,23,15,14,12,18,19,20,21,7,5]
 
 
-# 0 Black  # 1 Blue # 2 Yellow # 3 Green # 4 Orange # 5 Red
->>> ColorsInitial = [4,2,1,0,3,2,1,5,4,3,2,5,5,0,1,4,0,1,3,4,0,5,2,3]
->>> ErrorInitial = sum([len(set(Colors[i*4:i*4+4])) for i in range(6)])
->>> ErrorInitial
->>> 24
-
-def rubik_solve_rnd(I, max_attempts=30, max_steps=6,random_spins=3000,max_spins=10,min_spins=4):
+def rubik_solve_rnd(colors=[4,2,1,0,3,2,1,5,4,3,2,5,5,0,1,4,0,1,3,4,0,5,2,3], atempts_max=30, sequences_max=10,random_rotations=3000,min_steps=1,max_steps=10):
   """
   Help on rubik solver function
   
   When you provide the current colors on the cube the function runs random emulations 
     of cube rotation series and evaulates the outcome.
   
-  I: is the initial list of colors, the starting position
-  max_attemps: number of complete restarts (if attempt is not successful)
+  Colors: is the initial list of colors (from left to right, from top to down), default is the completely scrambled cube
+  Attempts: number of complete restarts (if an attempt is not successful)
   max_seqs: maximum number of evaluations
-  random_spins: number of random transformation series per iteration
+  random_rotations: number of random transformation series per iteration
   max_spins: maximum lenght of an evaluation sequence
   min_spins: minimum lenght of an evaluation sequence
   """
-  for attempt in range(max_attempts):
-    It = I
-    Ei = sum([len(set(It[i*4:i*4+4])) for i in range(6)]) #calculated from initial
-    print("\n"+"\n"+"-------"+"Attempt--"+str(attempt)+"........."+"init error: "+str(Ei), flush=True)
-    Ts = []           #successful rotation series to be collected
-    for step in range(max_steps): #transformations starting from initial setup
-      E = sum([len(set(It[i*4:i*4+4])) for i in range(6)])
-      #for spins in range(max_spins,min_spins,-1): #gradually decrease! (so prefer longer ones) the number of spins between evaluations
-      for spins in range(min_spins,max_spins):
+  for atempt in range(atempts_max):
+    It = colors 
+    Ei = sum([len(set(It[i*4:i*4+4])) for i in range(6)]) #Error initial
+    print("\n"+"\n"+"-------"+"Atempt--"+str(atempt)+"........."+"init error: "+str(Ei), flush=True)
+    Ts = []           #Collect successful transformation sequences
+    for sequence in range(sequences_max):
+      E = sum([len(set(It[i*4:i*4+4])) for i in range(6)]) #Error before rotation(s)
+      for steps in range(min_steps,max_steps):
         found = False
-        for random_spin in range(random_spins): #try random spins many times per each length (i)
+        for random_rotation in range(random_rotations): #try random spins many times per each length (i)
           Itt = It
           Tst = []
-          for k in range(spins):
+          for k in range(steps):
             tt = Tk[random.choice(range(12))]    #random selection of the transformation
             Itt = list(itemgetter(*T[tt])(Itt)) #transformation applied -> Itt
             Tst.append(tt)
@@ -62,9 +55,9 @@ def rubik_solve_rnd(I, max_attempts=30, max_steps=6,random_spins=3000,max_spins=
             found = True
             It = Itt        #accept the transformations sequence (1-10 steps long)
             Ts.append(Tst)  #store the successful sequence
-            print("STEP-"+str(step)+" ERROR: " + str(Et)+" seqlen-"+str(spins)+" iteration-"+str(random_spin), flush=True)
+            print("Sequence-"+str(sequence)+" ERROR: " + str(Et)+" sequence length-"+str(steps)+" iteration-"+str(random_rotation), flush=True)
             break           # l loop - random search with i lenght - was successful
-        if Et<E:
+        if found:
           break  # minmax spins loop has to be interrupted as well!
       if Et==6:
         #print(Ts, flush=True)
@@ -72,6 +65,7 @@ def rubik_solve_rnd(I, max_attempts=30, max_steps=6,random_spins=3000,max_spins=
         for ts in Ts:
           for t in ts:A.append(t)
         
+        #remove redundancy like LeftUp -> LeftDown ... 
         for i in range(len(A)):
           for j in range(len(A)):
             if j<len(A)-1 and \
